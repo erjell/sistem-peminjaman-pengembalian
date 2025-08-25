aa5443a12927f5d4f539f49c25a3c3f58b2f46ea
erjell <68001861+erjell@users.noreply.github.com>
2025-08-25 21:56:49 +0700

    refactor: simplify mobile barang view

--- resources/views/mobile/barang.blade.php
+++ resources/views/mobile/barang.blade.php
@@ -1,13 +1,5 @@
 <x-app-layout>
-    <!DOCTYPE html>
-    <html lang="id">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Form Input Barang - Mobile</title>
-        <script src="https://cdn.tailwindcss.com"></script>
-    </head>
-    <body class="bg-gray-100 p-4">
+    <div class="bg-gray-100 p-4">
         <div class="max-w-md mx-auto bg-white p-4 rounded shadow">
             <h1 class="text-xl font-bold mb-4">Form Input Barang</h1>
 
@@ -154,6 +146,5 @@
             setTimeout(() => popup.classList.add('hidden'), 3000);
         }
         </script>
-    </body>
-    </html>
+    </div>
 </x-app-layout>
